01da8af6f130fea91e91b8177cb7105f238d6670
k-dang <dangkevin12@gmail.com>
2019-03-10 00:59:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var connectionCounter = 0;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	connectionCounter += 1;
+	console.log(`${connectionCounter} total users connected `);
+
+	socket.on('disconnect', function(){
+		connectionCounter -= 1;
+	});
+
+	socket.on('url change', function(msg){
+		socket.broadcast.emit('url change', msg);
+	})
+
+	socket.on('chapter change', function(msg){
+		socket.broadcast.emit('chapter change', msg);
+	})
+
+	socket.on('page change', function(msg){
+		socket.broadcast.emit('page change', msg);
+	})
+
+	socket.on('page update', function(msg){
+		socket.broadcast.emit('page update', msg);
+	});
+
+	socket.on('chapter update', function(msg){
+		socket.broadcast.emit('chapter update', msg);
+	})
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+})
